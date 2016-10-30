fe8d5a68dd86197f6a41268e34814ff90b779779
Alec Littlechild <alec.littlechild@gmail.com>
2016-10-30 11:27:10 +0100

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,28 @@
+# Week 1 'exploratory data analysis', plot 2
+
+require(sqldf)
+localFile <- "household_power_consumption.txt"
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+destfile <- "household_power_consumption.zip"
+
+if (!file.exists(localFile)){
+  download.file(url,destfile)
+  unzip(destfile)
+  unlink(destfile)
+}
+
+#read only the required dates (2007-02-01 and 2007-02-02)
+library(sqldf)
+inputData <- read.csv2.sql(localFile,
+                           header=TRUE,
+                           sep=";",
+                           na.strings="?",
+                           sql = "select * from file where Date = '1/2/2007' or Date = '2/2/2007'")
+
+inputData$DateTime <- paste(inputData$Date,inputData$Time)
+inputData$DateTime <- strptime(inputData$DateTime,format="%d/%m/%Y %H:%M:%S") #1/1/2007 00:01:00
+inputData$Date <- as.Date(inputData$Date,format="%d/%m/%Y")
+
+png("plot2.png",width=480,height=480)
+with(inputData,plot(DateTime,Global_active_power,type="l",ylab="Global Active Power (kilowatts)"))
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,41 @@
+# Week 1 'exploratory data analysis', plot 3
+
+require(sqldf)
+localFile <- "household_power_consumption.txt"
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+destfile <- "household_power_consumption.zip"
+
+if (!file.exists(localFile)){
+  download.file(url,destfile)
+  unzip(destfile)
+  unlink(destfile)
+}
+
+#read only the required dates (2007-02-01 and 2007-02-02)
+library(sqldf)
+inputData <- read.csv2.sql(localFile,
+                           header=TRUE,
+                           sep=";",
+                           na.strings="?",
+                           sql = "select * from file where Date = '1/2/2007' or Date = '2/2/2007'")
+
+inputData$DateTime <- paste(inputData$Date,inputData$Time)
+inputData$DateTime <- strptime(inputData$DateTime,format="%d/%m/%Y %H:%M:%S") #1/1/2007 00:01:00
+inputData$Date <- as.Date(inputData$Date,format="%d/%m/%Y")
+
+png("plot4.png",width=480,height=480)
+par(mfrow=c(2,2))
+#plot1
+with(inputData, plot(DateTime,Global_active_power,ylab="Global Active Power",type="l"))
+#plot2 voltage
+with(inputData, plot(DateTime,Voltage,xlab="datetime",ylab="Voltage",type="l"))
+
+#plot 3
+with(inputData, plot(DateTime,Sub_metering_1,type="l",col="black",ylab="Energy sub metering"))
+with(inputData, lines(DateTime,Sub_metering_2,col="red"))
+with(inputData, lines(DateTime,Sub_metering_3,col="blue"))
+legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("black","red","blue"),lwd=1,bty="n")
+
+#plot4
+with(inputData,plot(DateTime,Global_reactive_power,type="l",xlab="datetime"))
+dev.off()
